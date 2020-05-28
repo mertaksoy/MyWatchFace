6472a50457f437517fc68e6eb0b47a6b237e90d2
Mert Aksoy <mert.aksoy@codesquare.at>
2020-05-28 13:09:40 +0200

    Initializing tizen watch face project

---
+++ inc/mywatch.h
@@ -0,0 +1,18 @@
+#ifndef __mywatch_H__
+#define __mywatch_H__
+
+#include <watch_app.h>
+#include <watch_app_efl.h>
+#include <Elementary.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "mywatch"
+
+#if !defined(PACKAGE)
+#define PACKAGE "at.codesquare.mywatch"
+#endif
+
+#endif /* __mywatch_H__ */
